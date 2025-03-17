3a16f1835cb194bd3743c68412fed99b6949ed84
konsch12 <konstanze.lauseker@live.at>
2025-03-17 12:08:17 +0100

    Advanced Ex 2 created

---
+++ BOKU Exams/examples/ad2_stories.R
@@ -0,0 +1,20 @@
+#TODO fill out other stories
+
+stories <- list( # stories (which are lists themselves) are stored in a list
+  sun_plant = list(
+    text = "An experiment was conducted to study the effects sun exposure on the height of tomato plants. Each plant was exposed to the sunlight for a certain period of time a day. After 30 days, their height was recorded.", 
+    var = c("sun exposure", "height of tomato plants"), 
+    unit = c("h", "cm"), 
+    mu = 5, #needed for generating data 
+    sigma = 1
+  ),
+  
+  wind_energy = list(
+    text = "On several days the wind speed was recorded and the ouput of wind turbines was recorded"
+  )
+  
+  
+  
+  
+  
+)
